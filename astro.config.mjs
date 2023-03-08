10625ae29eb6317907a352bbed75444502c37fb1
Paul <paul@theapplegates.com>
2023-03-08 17:01:36 -0500

    first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,9 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+	experimental: {
+	  assets: true
+	 }
 	site: 'https://example.com',
 	integrations: [mdx(), sitemap()],
 });
